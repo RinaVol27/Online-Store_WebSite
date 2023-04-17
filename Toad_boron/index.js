0c425ce0eb1646a230f963b849dbad4b1028573a
RinaVol27 <130033747+RinaVol27@users.noreply.github.com>
2023-04-17 16:54:39 +0300

    hjk

--- Toad_boron/index.js
+++
@@ -1,5 +0,0 @@
-async function getAllProducts(){
-    var result = await fetch("https://localhost:7003/api/Product");
-    console.log(result);
-}
-getAllProducts();
---
+++ Toad_boron/product_card.js
@@ -0,0 +1,14 @@
+async function getAllProducts(){
+    var result = await fetch("https://localhost:7003/api/Product");
+    console.log(result);
+
+    if (result.status !== 200){
+        console.log('Error: ' + result.status);
+        return;
+    }
+
+    var data = await result.json();
+    for (let i=0; i < data.lenght; i++){
+        console.log(data[i]);
+    }
+}
