5ab4729bc1724eba247062d0f8096eb92ca1b3af
RinaVol27 <130033747+RinaVol27@users.noreply.github.com>
2023-04-17 17:30:33 +0300

    update

--- Toad_boron/product_card.js
+++ Toad_boron/product_card.js
@@ -10,7 +10,22 @@ async function getAllProducts(){
     var data = await result.json();
     for (let i=0; i < data.lenght; i++){
         console.log(data[i]);
+        addProductCard(data[i]);
     }
+}
+function addProductCard(product){
+    let list = document.getElementById('products-list');
+
+    let card = document.createElement('div');
+    card.className = "card";
+
+    card.innerHTML = '<img src="' + product.url + '"alt="Denim Jeans" style="width:100%>' +
+    '<h1>' + product.name + '</h1>' +
+    '<p class="price">' + product.price + '</p>' +
+    '<p>' + product.desription + '</p>' +
+    '<p><button>Add to Cart</button></p>'
+
+    list.appendChild(card);
 }
 
 getAllProducts();
